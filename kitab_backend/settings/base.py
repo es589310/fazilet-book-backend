a479d6875a2d94ec485c4c920960ded84717bcf4
mr.panahov <panahovemin10@gmail.com>
2025-08-17 01:51:58 +0400

    Update Django settings to read .env.local for local development

--- kitab_backend/settings/base.py
+++ kitab_backend/settings/base.py
@@ -10,6 +10,7 @@ import dj_database_url
 
 env = environ.Env()
 environ.Env.read_env()  # local .env üçün
+environ.Env.read_env('.env.local')  # local .env.local üçün
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
