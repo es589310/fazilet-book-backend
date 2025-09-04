2c02b090eaed6d8a96bfa51caf93a3e3d16c6927
mr.panahov <panahovemin10@gmail.com>
2025-09-04 13:46:17 +0400

    Banner title və subtitle sahələrini məcburi olmadan dəyişdir

--- books/models.py
+++ books/models.py
@@ -189,8 +189,8 @@ class BookReview(models.Model):
 from django.db import models
 
 class Banner(models.Model):
-    title = models.CharField(max_length=200, verbose_name="Başlıq")
-    subtitle = models.CharField(max_length=300, blank=True, verbose_name="Alt başlıq")
+    title = models.CharField(max_length=200, blank=True, null=True, verbose_name="Başlıq")
+    subtitle = models.CharField(max_length=300, blank=True, null=True, verbose_name="Alt başlıq")
     image = models.ImageField(upload_to='banners/', blank=True, null=True, verbose_name="Şəkil")
     imagekit_url = models.URLField(blank=True, null=True, verbose_name="ImageKit URL")
     imagekit_id = models.CharField(max_length=100, blank=True, null=True, verbose_name="ImageKit ID")
@@ -203,6 +203,6 @@ class Banner(models.Model):
         verbose_name_plural = "Reklam Panoları"
 
     def __str__(self):
-        return self.title
+        return self.title or f"Banner {self.id}"
 
 
